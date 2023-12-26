f490449a25fc0851d9d42d0aa02631d9406c2853
Vraj patel <vraj.patel4801@gmail.com>
2023-12-26 19:43:01 +0530

    change token name for production mode and for development/local mode in middlewares:)

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const isAuth = () => request.cookies.has("next-auth.session-token");
+  const isAuth = () =>
+    process.env.NODE_ENV === "production"
+      ? request.cookies.has(process.env.GOOGLE_TOKEN_PRODUCTION)
+      : request.cookies.has(process.env.GOOGLE_TOKEN_LOCAL);
 
   if (isAuth()) return NextResponse.next();
   return NextResponse.redirect(new URL("/unauhtorized", request.url));
