a415181c2979c9f4f6ef76b5079b0f48ebc63ae7
Vraj patel <vraj.patel4801@gmail.com>
2023-12-26 18:24:20 +0530

    middleware content is changed:)

--- src/middleware.js
+++ src/middleware.js
@@ -1,24 +1,17 @@
 import { NextResponse } from "next/server";
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request) {
-  //
   const isAuth = () => request.cookies.has("next-auth.session-token");
   const pathname = request.nextUrl.pathname;
 
   // middleware for pages access
   if (pathname.startsWith("/users") || pathname.startsWith("/blogs")) {
-    return isAuth() ? NextResponse.next() : NextResponse.error();
+    return isAuth() ? NextResponse.next() : NextResponse.error({ status: 401 });
   }
 
-  return NextResponse.error();
+  return NextResponse.error({ status: 404 }); // Adjust the status code as needed
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    //pages
-    "/users/:path*",
-    "/blogs/:path*",
-  ],
+  matcher: ["/users/:path*", "/blogs/:path*"],
 };
